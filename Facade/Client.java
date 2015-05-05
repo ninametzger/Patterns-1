f2929083fc51462f81e49f5daf2788f1e7e953b3
ninametzger <nina-metzger@arcor.de>
2015-05-05 20:45:52 +0200

    Update Client.java

--- Facade/Client.java
+++ Facade/Client.java
@@ -4,7 +4,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		OrderFacade facade = new OrderFacade();
-		facade.order("2342");
+		System.out.println(facade.order("2342"));
 		System.out.println("Process done!");
 	}
 }
